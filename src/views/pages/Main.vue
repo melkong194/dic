560d512064f44f671a4638a931cecd03bf961636
Melko <melkong194@gmail.com>
2022-03-18 02:38:04 +0930

    Fixing playing

--- src/views/pages/Main.vue
+++ src/views/pages/Main.vue
@@ -719,6 +719,7 @@ export default defineComponent({
 			// this.sorted = [];
 			//update word list with new sylls into database
 			this.initial();
+            this.updateEvent(true);
 		},
 		handleRecord() {
 			// const record = document.querySelector('.startRecord');
